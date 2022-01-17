ee95dfd3846bea863c1a85c3db8feac655ebd53c
mirzabekov00 <prog.eldar@gmail.com>
2022-01-17 21:43:06 +0200

    feat(user-model): add user db model

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { User } from "./users/users.model";
 import { UsersModule } from "./users/users.module";
 
 @Module({
@@ -19,7 +20,7 @@ import { UsersModule } from "./users/users.module";
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
