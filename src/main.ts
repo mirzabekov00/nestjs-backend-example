958c2dedc05008c53018f5e63541efc926236623
mirzabekov00 <prog.eldar@gmail.com>
2022-01-17 22:31:13 +0200

    feat(users): add open-api, documented all routes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle("nest.js example backend application")
+    .setDescription("rest api documentation")
+    .setVersion("1.0.0")
+    .addTag("mirzabekov")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api/docs", app, document);
+
   await app.listen(PORT);
 
   const appUrl = await app.getUrl();
 
-  console.log(`Application is running on: ${appUrl}`);
+  console.log(`application is running on: ${appUrl}`);
 }
 bootstrap();
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface UserCreationAttributes {
@@ -7,6 +8,7 @@ interface UserCreationAttributes {
 
 @Table({ tableName: "users" })
 export class User extends Model<User, UserCreationAttributes> {
+  @ApiProperty({ example: 1, description: "unique, primary user id" })
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -15,6 +17,7 @@ export class User extends Model<User, UserCreationAttributes> {
   })
   id: number;
 
+  @ApiProperty({ example: "eldar@gmail.com", description: "user e-mail" })
   @Column({
     type: DataType.STRING,
     unique: true,
@@ -22,18 +25,21 @@ export class User extends Model<User, UserCreationAttributes> {
   })
   email: string;
 
+  @ApiProperty({ example: "test777", description: "user password" })
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
   password: string;
 
+  @ApiProperty({ example: true, description: "is user banned" })
   @Column({
     type: DataType.BOOLEAN,
     defaultValue: false,
   })
   banned: boolean;
 
+  @ApiProperty({ example: "trolling", description: "is user banned" })
   @Column({
     type: DataType.STRING,
     allowNull: true,
