1797df97c068c1938946f19480286b6ae734a86a
mirzabekov00 <prog.eldar@gmail.com>
2022-01-19 21:45:52 +0200

    feat(guards): add user-role guard check

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { Roles } from "src/auth/roles.decorator";
+import { RolesGuard } from "src/auth/roles.guard";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./users.model";
 import { UsersService } from "./users.service";
@@ -19,6 +21,8 @@ export class UsersController {
 
   @ApiOperation({ summary: "get all users" })
   @ApiResponse({ status: 200, type: [User] })
+  @Roles("admin")
+  @UseGuards(RolesGuard)
   @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
