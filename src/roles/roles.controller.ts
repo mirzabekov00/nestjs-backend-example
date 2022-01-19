08b2eca9e821736c1dda200111c225de3f99d44b
mirzabekov00 <prog.eldar@gmail.com>
2022-01-19 22:14:40 +0200

    chore(roles): add documentation for roles

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,21 +1,29 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreateRoleDto } from "./dto/create-role.dto";
 import { RolesService } from "./roles.service";
 
+@ApiTags("roles")
 @Controller("roles")
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
+  @ApiOperation({ summary: "create role for user" })
+  @ApiResponse({ status: 200 })
   @Post()
   create(@Body() roleDto: CreateRoleDto) {
     return this.rolesService.createRole(roleDto);
   }
 
+  @ApiOperation({ summary: "get role info by value" })
+  @ApiResponse({ status: 200 })
   @Get("/:value")
   getByValue(@Param("value") value: string) {
     return this.rolesService.getRoleByValue(value);
   }
 
+  @ApiOperation({ summary: "get all roles" })
+  @ApiResponse({ status: 200 })
   @Get()
   getAll() {
     return this.rolesService.getAllRoles();
